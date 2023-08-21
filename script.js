4fa435605e5e13e1b253083c32773c3289eb7ab0
cantaronamelathil <cantaronamelathil@gmail.com>
2023-08-21 13:30:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function generate(){
+    var quotes={
+        "- Albert Einstein":'"Our task must be to free ourselves...by widening our circle of compassion to embrace all living creatures and the whole of nature and its beauty."',
+        "- Pierce Brown":'"Man cannot be freed by the same injustice that enslaved it."'
+
+    }
+    var authors = Object.keys(quotes);
+    var author = authors[Math.floor(Math.random()*authors.length)]
+    var quote = quotes[author];
+    document.getElementById("quote").innerHTML=quote;
+    document.getElementById("author").innerHTML=author;
+}
+
